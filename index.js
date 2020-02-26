355131af7449231026336af9c767e13ebacd653b
Alex Trask <Sarlack_Work@mail.ru>
2020-02-26 18:18:40 +0300

    Добавил загрузку файлов

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const multer  = require('multer')
+const app = express()
 
-const PORT = process.env.PORT || 80;
+app.use(express.static(__dirname))
+app.use(multer({dest: 'requests'}).single('filedata'))
+
+const PORT = process.env.PORT || 80
 
 app.get('/', (req, res) => {
-    res.json({message: `API has been initialized!`});
+
+    //console.log(req.query)
+
+    //res.json({message: `API has been initialized!`})
+    res.sendFile('index.html')
+})
+
+
+
+app.post("/requests", function (req, res, next) {
+   
+    const filedata = req.file;
+    console.log(filedata);
+    if(!filedata)
+        res.send("Ошибка при загрузке файла");
+    else
+        res.send("Файл загружен");
 })
 
 app.listen(PORT, () => {
-    console.log('Сервер работает');
+    console.log('Сервер работает')
 })
