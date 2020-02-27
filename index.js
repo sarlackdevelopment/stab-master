1614453e978c2d76c475f8c773689970e1398fee
Alex Trask <Sarlack_Work@mail.ru>
2020-02-27 20:57:46 +0300

    autorisation is done

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const multer  = require('multer')
 const mkdirp = require('mkdirp');
+const bodyParser = require("body-parser");
 
 const app = express()
 
+//delete (app.cache)
+
 app.use(express.static(__dirname))
+//app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+//console.log(__dirname)
 
 const PORT = process.env.PORT || 8080
+
+//const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.listen(PORT, () => {
     console.log('Сервер работает')
 })
+
+//users = [{login: 'Volodya', password: 'pK893zcDhMYM'}]
+users = [{login: 'a', password: '1'}]
 
 const createStorage = (destanation) => {
   return multer.diskStorage({
@@ -29,7 +41,25 @@ requestsUpload = multer({ storage: createStorage('requests') })
 responsesUpload = multer({ storage: createStorage('responses') })
 responsesСonfigs = multer({ storage: createStorage('configs') })
 
-app.get('/', (req, res) => res.sendFile('index.html'))
+//app.get('/', (req, res) => {
+  //res.sendFile('/login.html')
+  //console.log('123123123')
+  //response.send("<h2>Привет Express!</h2>");
+//})
+
+app.post('/login', (req, res) => {
+  const user = users.find(({login, password}) => 
+    login === req.body.login && password === req.body.password)
+
+  if (user == undefined) {
+    res.sendStatus(400)
+  } else {
+    res.sendFile('login.html', { root: __dirname })
+  }
+
+})
+
+//app.get('/', (req, res) => res.sendFile('index.html'))
 
 app.post('/requests', requestsUpload.any(), (req , res) => {
   res.redirect("/")
