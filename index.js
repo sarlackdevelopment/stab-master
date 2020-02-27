db5115f1524df5acd3f9c95255f416679a15ff96
Alex Trask <Sarlack_Work@mail.ru>
2020-02-27 21:15:21 +0300

    customize redirect is done

--- index.js
+++ index.js
@@ -5,17 +5,10 @@ const bodyParser = require("body-parser");
 
 const app = express()
 
-//delete (app.cache)
-
 app.use(express.static(__dirname))
-//app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-//console.log(__dirname)
-
 const PORT = process.env.PORT || 8080
-
-//const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.listen(PORT, () => {
     console.log('Сервер работает')
@@ -41,36 +34,29 @@ requestsUpload = multer({ storage: createStorage('requests') })
 responsesUpload = multer({ storage: createStorage('responses') })
 responsesСonfigs = multer({ storage: createStorage('configs') })
 
-//app.get('/', (req, res) => {
-  //res.sendFile('/login.html')
-  //console.log('123123123')
-  //response.send("<h2>Привет Express!</h2>");
-//})
-
 app.post('/login', (req, res) => {
   const user = users.find(({login, password}) => 
     login === req.body.login && password === req.body.password)
 
   if (user == undefined) {
-    res.sendStatus(400)
+    res.redirect("/error.html")
   } else {
-    res.sendFile('login.html', { root: __dirname })
+    //res.sendFile('login.html', { root: __dirname })
+    res.redirect("/login.html")
   }
 
 })
 
-//app.get('/', (req, res) => res.sendFile('index.html'))
-
 app.post('/requests', requestsUpload.any(), (req , res) => {
-  res.redirect("/")
+  res.redirect("/login.html")
 })
 
 app.post('/responses', responsesUpload.any(), (req , res) => {
-  res.redirect("/")
+  res.redirect("/login.html")
 })
 
 app.post('/configs', responsesСonfigs.any(), (req , res) => {
-  res.redirect("/")
+  res.redirect("/login.html")
 })
 
 app.get('/some', (req, res) => {
