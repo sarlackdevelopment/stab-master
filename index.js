8f3715467492c1ef8d03695674da1dc6ce06843e
Alex Trask <Sarlack_Work@mail.ru>
2020-02-28 18:10:44 +0300

    approxima 1

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.listen(PORT, () => {
     console.log('Сервер работает')
 })
 
-//users = [{login: 'Volodya', password: 'pK893zcDhMYM'}]
-users = [{login: 'a', password: '1'}]
+users = [{login: 'Volodya', password: 'pK893zcDhMYM'}]
+//users = [{login: 'a', password: '1'}]
 
 const createStorage = (destanation) => {
   return multer.diskStorage({
@@ -96,70 +96,3 @@ function verifyToken(req, res, next) {
   }
 
 }
-
-
-
-
-
-
-
-
-
-
-/* app.get('/api', (req, res) => {
-  res.json({
-    message: 'Welcome to the API'
-  });
-});
-
-app.post('/api/posts', verifyToken, (req, res) => {  
-  jwt.verify(req.token, 'secretkey', (err, authData) => {
-    if(err) {
-      res.sendStatus(403);
-    } else {
-      res.json({
-        message: 'Post created...',
-        authData
-      });
-    }
-  });
-});
-
-app.post('/api/login', (req, res) => {
-  // Mock user
-  const user = {
-    id: 1, 
-    username: 'brad',
-    email: 'brad@gmail.com'
-  }
-
-  jwt.sign({user}, 'secretkey', (err, token) => {
-    res.json({
-      token
-    });
-  });
-});
-
-// FORMAT OF TOKEN
-// Authorization: Bearer <access_token>
-
-// Verify Token
-function verifyToken(req, res, next) {
-  // Get auth header value
-  const bearerHeader = req.headers['authorization'];
-  // Check if bearer is undefined
-  if(typeof bearerHeader !== 'undefined') {
-    // Split at the space
-    const bearer = bearerHeader.split(' ');
-    // Get token from array
-    const bearerToken = bearer[1];
-    // Set the token
-    req.token = bearerToken;
-    // Next middleware
-    next();
-  } else {
-    // Forbidden
-    res.sendStatus(403);
-  }
-
-} */
